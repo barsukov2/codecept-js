4a180fd951eb400db611ef519204374784d0923d
Barsukov <a.barsukov@rabota.ru>
2022-07-31 02:24:36 +0400

    update recentlyViewed.js test

--- specs/recentlyViewed.js
+++ specs/recentlyViewed.js
@@ -3,9 +3,9 @@ var productTitle = '';
 Before (({I, homePage}) => {
     I.amOnPage('/');
     productTitle = homePage.goToRandomFeaturedProduct();
-    I.amOnPage('/');
 });
 
 Scenario('После просмотра товара он появляется в блоке Recently viewed', async({ I, homePage }) => {
+    I.amOnPage('/');
     homePage.seeProductInRecentlyViewed(productTitle);
 });
